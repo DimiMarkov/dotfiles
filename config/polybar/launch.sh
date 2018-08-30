b0472db43221f26f19dc8e4b84ba3ed879c84e10
Kevin Wiesmueller <kwiesmueller@seibert-media.net>
2018-08-30 16:24:07 +0200

    change to different polybar design

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -10,10 +10,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ $SCREENS -gt 1 ]; then
-  polybar left &
-  polybar right &
+  polybar -c ~/.config/polybar/config.ini left &
+  polybar -c ~/.config/polybar/config.ini right &
 fi
 
-polybar primary &
+polybar -c ~/.config/polybar/config.ini primary &
 
 echo "Bars launched..."
